a3fced1fdb7762b2bb674a848033ff6507c37776
sunjoolee <86330886+sunjoolee@users.noreply.github.com>
2023-03-10 08:51:01 +0900

    프로그래머스 카드 뭉치

--- sunjoolee/프로그래머스/159994.cpp
+++ sunjoolee/프로그래머스/159994.cpp
@@ -10,18 +10,14 @@ string solution(vector<string> cards1, vector<string> cards2, vector<string> goa
     int idx2 = 0;
     for(int i = 0; i<goal.size(); ++i){
         //카드 뭉치 1에서 가져올 수 있는 경우
-        if(idx1 != cards1.size()){
-            if(cards1[idx1] == goal[i]){
-                idx1++;
-                continue;
-            }
+        if((idx1 != cards1.size()) &&(cards1[idx1] == goal[i])){
+            idx1++;
+            continue;
         }
         //카드 뭉치 2에서 가져올 수 있는 경우
-        if(idx2 != cards2.size()){
-            if(cards2[idx2] == goal[i]){
-                idx2++;
-                continue;
-            }
+        if((idx2 != cards2.size())&&(cards2[idx2] == goal[i])){
+            idx2++;
+            continue;
         }
         //불가능
         return "No";
